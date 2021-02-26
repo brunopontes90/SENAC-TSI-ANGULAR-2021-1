2b35c484907ad9439a5aef981aa29fb91598985b
Bruno Pontes <bruno_confessor@hotmail.com>
2021-02-25 22:48:55 -0300

    aula 2 + exercicios

--- Aula 1/exercicios/exercicio02.ts
+++ Aula 1/exercicios/exercicio02.ts
@@ -5,6 +5,6 @@ let n1 = parseFloat(rs.question('1º Nota: '));
 let n2 = parseFloat(rs.question('2º Nota: '));
 let n3 = parseFloat(rs.question('3º Nota: '));
 
-let media = (n1 + n2 + n3) / 3;
+let media = ((n1 * 0.20)+ (n2 * 0.30) + (n3 * 0.50)) / 3;
 
 console.log(`Sua media final é: ${media}`);
