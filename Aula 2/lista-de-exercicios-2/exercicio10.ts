6cf7405fc69f1001ce8a42a69f3439be81d3065f
Bruno Pontes <bruno_confessor@hotmail.com>
2021-03-17 17:24:36 -0300

    exercicios aula 4

--- Aula 2/lista-de-exercicios-2/exercicio10.ts
+++ Aula 2/lista-de-exercicios-2/exercicio10.ts
@@ -1,6 +1,12 @@
 import * as rs from 'readline-sync';
 
-let palavra: string;
-palavra = rs.question('Digite uma palavra que comece com "P" e contenha "F": ');
-palavra.length;
-console.log(palavra);
+let word = String(rs.question('Digite uma palavra que comece com "P" e contenha "F": ').toUpperCase());
+
+if(word.indexOf('F') !== -1 && word[0] == 'P'){
+    console.log(`A palavra começa com a letra "P" na posição: ${word.indexOf('P', 0)}`);
+    console.log(`A palavra possui 'F' na posição: ${word.indexOf('F')}`);
+}else {
+    console.log('A palavra não começa com a letra "P"');
+    console.log('A palavra não possui "F"');
+}
+
