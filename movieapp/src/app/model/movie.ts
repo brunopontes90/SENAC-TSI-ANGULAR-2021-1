4d9482e4193df0d50e8c02c9b2eaf1f5cb65b657
Bruno Pontes <bruno_confessor@hotmail.com>
2021-04-11 00:31:36 -0300

    Classes de modelo, CRUD

---
+++ movieapp/src/app/model/movie.ts
@@ -0,0 +1,14 @@
+// RATING?: NUMBER
+// ? NÃO SERA OBRIGATORIO
+// PODE USAR ANY OU 0 OU NUMBER | UNDEFINED | ANY
+export class Movie{
+  name: string;
+  rating: number;
+
+  constructor(name: string, rating: number){
+    this.name = name;
+    this.rating = rating;
+  }
+}
+
+// GUARDA DOS DADOS QUE ESTA NA MEMORIA
